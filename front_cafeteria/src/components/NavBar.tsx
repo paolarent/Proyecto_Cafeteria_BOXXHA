3d28e49bd5ff911d32ea77912265bf0aff91c4fc
paolarent <prenteriaf@gmail.com>
2025-04-14 13:16:01 -0700

    Primer respaldo del front

---
+++ front_cafeteria/src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logom from "../assets/logo_menu.png";
+import icon_user from "../assets/icon_usuario.png"
+import icon_like from "../assets/icon_like.png"
+import icon_carrito from "../assets/icon_carrito.png"
+
+
+const NavBar = () => {
+    return (
+        <div className="w-full flex flex-col">
+            {/* Barra de navegación superior */}
+            <nav className="flex justify-between items-center p-4 border-b border-gray-200">
+                <div className="flex items-center h-10 space-x-2">
+                    <button className="flex items-center space-x-2 hover:scale-105 transition">
+                        <img src={logom} alt="Logo" className="h-14" />
+                        <span className="text-xl font-extrabold font-serif">BOXXHA CAFÉ</span>
+                    </button>
+                </div>
+                <ul className="flex space-x-8 text-sm font-Montserrat font-medium text-black-700">
+                    <li><button className="text-lg hover:[color:#A1C99C] transition">Inicio</button></li>
+                    <li><button className="text-lg hover:[color:#A1C99C] transition">Nosotros</button></li>
+                    <li><button className="text-lg hover:[color:#A1C99C] transition">Shop</button></li>
+                    <li><button className="text-lg hover:[color:#A1C99C] transition">Menú</button></li>
+                    <li>
+                        <button>
+                        <img src={icon_user} alt="Usuario" className="h-7 hover:scale-110 transition" />
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                        <img src={icon_like} alt="Favorito" className="h-7 hover:scale-110 transition" />
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                        <img src={icon_carrito} alt="Carrito" className="h-7 hover:scale-110 transition" />
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+};
+
+export default NavBar;
