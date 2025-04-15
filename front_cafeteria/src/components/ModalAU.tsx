1b525e6dba823811922dba6e6c9684be26b254a5
paolarent <prenteriaf@gmail.com>
2025-04-14 23:11:21 -0700

    "arreglo del modal", le hace falta el diseño pero ya esta centrado

--- front_cafeteria/src/components/ModalAU.tsx
+++ front_cafeteria/src/components/ModalAU.tsx
@@ -9,7 +9,7 @@ const ModalAU: React.FC<ModalAUProps> = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 w-[600px] h-[400px]">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white p-6 rounded-lg max-w-md w-full shadow-lg relative">
                 <button
                 onClick={onClose}
